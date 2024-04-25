53b5e65eaee8c37d4ba86e434d7b874f4a62acbc
Grigorchuk Kate <1416katerina@gmail.com>
2024-04-25 16:39:32 +0300

    Змінено клас PositiveNumberDeleter

--- src/main/java/onmu/kn/PositiveNumberDeleter.java
+++ src/main/java/onmu/kn/PositiveNumberDeleter.java
@@ -2,30 +2,25 @@ package onmu.kn;
 
 public class PositiveNumberDeleter {
     public static int[][] deletePositiveNumbers(int[] array) {
-        // Count the number of positive elements
+        // Підрахувати кількість позитивних елементів
         int count = 0;
         for (int num : array) {
             if (num > 0) {
                 count++;
-            }
-        }
-
-        // Create arrays to store non-positive and positive elements
+            } }
+        // Створення масивів для зберігання не додатних і додатних елементів
         int[] nonPositiveNumbers = new int[array.length - count];
         int[] positiveNumbers = new int[count];
         int indexNonPositive = 0;
         int indexPositive = 0;
-
-        // Copy non-positive and positive elements to the respective arrays
+        // Копіювати непозитивні та позитивні елементи у відповідні масиви
         for (int num : array) {
             if (num <= 0) {
                 nonPositiveNumbers[indexNonPositive++] = num;
             } else {
                 positiveNumbers[indexPositive++] = num;
-            }
-        }
-
-        // Return the arrays containing non-positive and positive numbers
+            } }
+        // Повертає масиви, що містять недодатні та додатні числа
         return new int[][] {nonPositiveNumbers, positiveNumbers};
     }
 }
