56ed414923503eb5c15f20ec3977071efb50a970
Emilia Yevtushok <emily12234828@gmail.com>
2024-04-25 15:55:17 +0200

    Changes in View of GUI

--- src/main/java/onmu/kn/PositiveNumberDeleter.java
+++ src/main/java/onmu/kn/PositiveNumberDeleter.java
@@ -2,24 +2,21 @@ package onmu.kn;
 
 public class PositiveNumberDeleter {
     public static int[] deletePositiveNumbers(int[] array) {
-        // Count the number of positive elements
-        int count = 0;
+        int number = 0;
         for (int num : array) {
             if (num > 0) {
-                count++;
+                number++;
             }
         }
-        // Create a new array with size equal to the count of non-positive elements
-        int[] result = new int[array.length - count];
-        int index = 0;
 
-        // Copy non-positive elements to the result array
+        int[] resultat = new int[array.length - number];
+        int index = 0; // Початковий ��ндекс результуючого м
         for (int num : array) {
             if (num <= 0) {
-                result[index++] = num;
+                resultat[index++] = num;
             }
         }
 
-        return result;
+        return resultat;
     }
 }
